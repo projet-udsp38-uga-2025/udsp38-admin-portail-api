dc3b0dcae80c307344b0d5871bfdd7315e7b5b1c
fagnigue <devfullcoul@gmail.com>
2025-11-06 13:54:00 +0100

    config: Configurer husky et commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+// commitlint.config.js
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'config',
+      ],
+    ],
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
+  },
+};
